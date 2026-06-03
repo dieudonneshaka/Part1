c68f4b3e2fb0e21d318a54f44a1e55c0b374aba0
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2026-06-03 16:23:01 +0300

    updated

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,41 +1,3 @@
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  )
-}
-
-const Part = ({ part, exercises }) => {
-  return (
-    <div>
-      <p>
-        {part} {exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = ({part1,exercises1,part2,exercises2,part3,exercises3,}) => {
-  return (
-    <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-    </div>
-  )
-}
-
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return (
-    <div>
-      <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
-      </p> 
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -47,21 +9,46 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-
-      <Content
-        part1={part1}exercises1={exercises1}
-        part2={part2}exercises2={exercises2}
-        part3={part3}exercises3={exercises3}
-      />
-
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
+
+const Header = (props) => {
+  return (
+    <div>
+      <Header course={course} />
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+} 
+
 
 export default App
