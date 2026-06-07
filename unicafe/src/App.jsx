ef0eaaf32b43291548cd3a2e03711d1e1f9fefc3
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2026-06-07 05:42:09 +0300

    unicafe exercise is done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,34 +1,56 @@
 import { useState } from 'react'
 
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
+  const handleGoodClick = () =>
+  setGood(good + 1)
 
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
+  const handleNeutralClick = () => 
+  setNeutral(neutral + 1)
+  
+  const handleBadClick = () => 
+  setBad(bad + 1)
+
+  const all = good + neutral + bad
+
+  const average = all === 0 ? 0 : (
+  good * 1 + neutral * 0 + bad * -1) / all
+
+  const positive = all === 0 ? 0 : (
+  good / all) * 100
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   )
 }
+
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average.toFixed(2)}</p>
+      <p>positive {positive.toFixed(2)} %</p>
+    </div>
+  )
+}
+
 
 export default App
